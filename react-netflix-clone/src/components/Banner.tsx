802161e47b11961473a72ec00ffbfeebee4b587e
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2025-10-22 14:38:33 +0900

    feat: enhance Banner component with styled-components and video playback functionality

--- react-netflix-clone/src/components/Banner.tsx
+++ react-netflix-clone/src/components/Banner.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import requests from '../api/requests';
 import instance from '../api/axios';
 import './Banner.css';
+import styled from 'styled-components';
 
 interface Movie {
   id: number;
@@ -10,10 +11,24 @@ interface Movie {
   original_name?: string;
   backdrop_path?: string;
   overview?: string;
+  videos?: {
+    results: {
+      key: string;
+      type: string;
+    }[];
+  };
+}
+
+function truncate(str: string | undefined, n: number) {
+  if (!str) {
+    return '';
+  }
+  return str?.length > n ? str.substring(0, n) + '...' : str;
 }
 
 export default function Banner() {
   const [movie, setMovie] = useState<Movie | null>(null);
+  const [isClicked, setIsClicked] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -33,38 +48,91 @@ export default function Banner() {
     setMovie(movieDetail);
   }
 
-  function truncate(str: string | undefined, n: number) {
-    if (!str) {
-      console.log('truncate: str is undefined!');
-      return;
-    }
-    return str?.length > n ? str.substring(0, n) : str;
-  }
-
-  return (
-    <header
-      className="banner"
-      style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-        backgroundPosition: 'top center',
-        backgroundSize: 'cover',
-      }}
-    >
-      <div className="banner_contents">
-        {/**TItle */}
-        <h1 className="banner_title">
-          {movie?.title || movie?.name || movie?.original_name}
-        </h1>
-        <div className="banner_buttons">
-          <button className="banner_button play">Play</button>
-          <button className="banner_button info">
-            <div className="space"></div> More Information
-          </button>
+  if (!isClicked) {
+    return (
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+          backgroundPosition: 'top center',
+          backgroundSize: 'cover',
+        }}
+      >
+        <div className="banner_contents">
+          {/**TItle */}
+          <h1 className="banner_title">
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
+          <div className="banner_buttons">
+            <button
+              className="banner_button play"
+              onClick={() => setIsClicked(true)}
+            >
+              Play
+            </button>
+            <button className="banner_button info">
+              <div className="space"></div> More Information
+            </button>
+          </div>
+          {/**DIV > 2 BUTTONS */}
+          <h1 className="banner_description">
+            {truncate(movie?.overview, 100)}
+          </h1>
+          {/**Description */}
         </div>
-        {/**DIV > 2 BUTTONS */}
-        <h1 className="banner_description">{truncate(movie?.overview, 10)}</h1>
-        {/**Description */}
-      </div>
-    </header>
-  );
+      </header>
+    );
+  } else {
+    const trailer = movie?.videos?.results.find(
+      (vid) => vid.type === 'Trailer',
+    );
+
+    const videoKey = trailer?.key;
+
+    return (
+      <Container>
+        <HomeContainer>
+          {videoKey ? (
+            <Iframe
+              src={`https://www.youtube.com/embed/${videoKey}?mute=1&autoplay=1&loop=1&playlist=${videoKey}`}
+              width="1280"
+              height="720"
+              allow="autoplay; fullscreen"
+            ></Iframe>
+          ) : (
+            <div>트레일러 영상 없음</div>
+          )}
+        </HomeContainer>
+      </Container>
+    );
+  }
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  border: none;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
