718dd2fda20f5844e195d38f447374206aa67aae
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2025-10-23 11:27:26 +0900

    feat: add Row component to display movie data with slider functionality

--- react-netflix-clone/src/App.tsx
+++ react-netflix-clone/src/App.tsx
@@ -1,11 +1,47 @@
+import requests from './api/requests';
 import Banner from './components/Banner';
 import Nav from './components/Nav';
+import Row from './components/Row';
+import './App.css';
 
 function App() {
   return (
     <div className="app">
       <Nav />
       <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rate" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
+      <Row
+        title="Horror Movies"
+        id="HM"
+        fetchUrl={requests.fetchHorrorMovies}
+      />
+      <Row
+        title="Romance Movies"
+        id="RM"
+        fetchUrl={requests.fetchRomaceyMovies}
+      />
+      <Row
+        title="Documantaries"
+        id="DM"
+        fetchUrl={requests.fetchDocumentaries}
+      />
     </div>
   );
 }
---
+++ react-netflix-clone/src/components/Row.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import instance from '../api/axios';
+import { type Movie } from '../types';
+import './Row.css';
+
+interface Props {
+  title: string;
+  fetchUrl: string;
+  id: string;
+  isLargeRow?: boolean;
+}
+
+const BASE_URL = 'https://image.tmdb.org/t/p/original';
+
+export default function Row({ title, fetchUrl, id, isLargeRow }: Props) {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    fetchMovieData();
+  }, [fetchUrl]);
+
+  const fetchMovieData = async () => {
+    const request = await instance.get(fetchUrl);
+    setMovies(request.data.results);
+    return request;
+  };
+
+  return (
+    <section className="row">
+      {/** TITLE */}
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider_arrow-left">
+          <span className="arrow">{'<'}</span>
+        </div>
+        <div id={id} className="row_posters">
+          {/**SEVERAL ROW_POSTER */}
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
+              src={`${BASE_URL}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              loading="lazy"
+              alt={movie.name}
+            />
+          ))}
+        </div>
+        <div className="slider_arrow-right">
+          <span className="arrow">{'>'}</span>
+        </div>
+      </div>
+    </section>
+  );
+}
