e06bda7c055f1660cea0a4341f8ad2b5972424f0
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2025-11-06 16:44:42 +0900

    fix: update logo click handler to navigate to home instead of reloading the page

--- react-netflix-clone/src/components/Nav.tsx
+++ react-netflix-clone/src/components/Nav.tsx
@@ -33,7 +33,7 @@ export default function Nav() {
         alt="Netflix logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
         className="nav_logo"
-        onClick={() => window.location.reload()}
+        onClick={() => window.location.assign('/')}
       />
       <input
         value={searchValue}
