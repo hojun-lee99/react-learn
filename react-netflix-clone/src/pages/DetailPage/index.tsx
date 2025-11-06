de9a8adc14d63ea8a18cba520ca6ac8ac14dd757
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2025-11-06 17:52:51 +0900

    feat: implement DetailPage component with movie data fetching and display

--- react-netflix-clone/src/pages/DetailPage/index.tsx
+++ react-netflix-clone/src/pages/DetailPage/index.tsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import type { Movie } from '../../types';
+import instance from '../../api/axios';
 
 export default function DeatilPage() {
-  return <div>DeatilPage</div>;
+  const { movieId } = useParams();
+  const [movies, setMovies] = useState<Movie | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await instance.get(
+        `https://api.themoviedb.org/3/movie/${movieId}`,
+      );
+      setMovies(request.data);
+    }
+    fetchData();
+  }, [movieId]);
+
+  if (!movies) return null;
+  return (
+    <section>
+      <img
+        className="modal_poster-img"
+        src={`https://image.tmdb.org/t/p/original/${movies.backdrop_path}}`}
+        alt="modal_poster-img"
+      />
+    </section>
+  );
 }
